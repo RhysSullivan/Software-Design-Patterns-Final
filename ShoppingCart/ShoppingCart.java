b285f7b9be5d09a9129eae261a1aada3c739e2b0
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2022-12-06 15:38:35 -0500

    Demo code

--- ShoppingCart/ShoppingCart.java
+++ ShoppingCart/ShoppingCart.java
@@ -3,33 +3,51 @@ package ShoppingCart;
 import java.util.ArrayList;
 import java.util.List;
 
+import ShoppingCart.Inventory.Inventory;
 import ShoppingCart.Payments.PaymentMethod;
 import ShoppingCart.Products.Product;
 
 public class ShoppingCart {
     // list of products in the cart
     private List<Product> products = new ArrayList<>();
+    private Inventory inventory;
+
+    public ShoppingCart(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
     // payment method for checkout
     private PaymentMethod paymentMethod;
 
     // observer list to dispatch actions
     private List<ActionObserver> observers = new ArrayList<>();
 
-    public void addProduct(Product product) {
-        products.add(product);
+    public void addProduct(int id) {
+        Product product = inventory.getInventoryItem(id);
+        if (product != null) {
+            products.add(product);
+        }
         dispatchAction(Action.ADD);
     }
 
     public void removeProduct(Product product) {
         products.remove(product);
+        inventory.addInventoryItem(product);
         dispatchAction(Action.REMOVE);
     }
 
     public void checkout() {
-        dispatchAction(Action.CHECKOUT);
+        var amountToPay = products.stream().mapToDouble(p -> p.getPrice()).sum();
+        if (paymentMethod.pay(amountToPay)) {
+            dispatchAction(Action.CHECKOUT);
+        }
     }
 
     public void clear() {
+        // add back to inventory
+        for (Product product : products) {
+            inventory.addInventoryItem(product);
+        }
         products.clear();
         dispatchAction(Action.CLEAR);
     }
--- main.java
+++ main.java
@@ -1,5 +1,13 @@
+import ShoppingCart.ShoppingCart;
+import ShoppingCart.Inventory.Inventory;
+import ShoppingCart.Payments.CreditCard;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.print("Hello World!");
+    Inventory inventory = new Inventory();
+    ShoppingCart cart = new ShoppingCart(inventory);
+    cart.setPaymentMethod(new CreditCard("1234567890123456", "123", "12/25", "John Doe"));
+    cart.addProduct(1);
+    cart.checkout();
   }
 }
